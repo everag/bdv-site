56cb218a552110c66d4de9212df3cb8104bd55fb
Everton Agner <ton.agner@gmail.com>
2020-09-07 12:39:14 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d2748596f811096c8b37.js"
+    "url": "webpack-runtime-58d8c19463dcbf914839.js"
   },
   {
     "url": "framework-b5b229d58c3c710d5429.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "55e050fcf9bb248bae904b96d192af6a"
+    "revision": "c181de1eff0241bc77fea634d49be146"
   },
   {
     "url": "google-fonts/s/bangers/v12/FeVQS0BTqb0h60ACH55Q2A.woff2",
